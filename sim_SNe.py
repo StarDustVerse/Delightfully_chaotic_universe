c4f424d2831cecf0a0fd2636ec2fda0cb4982908
ArchanaP <p.archana.ktr@outlook.com>
2025-08-22 15:06:14 +0530

    Update sim_SNe.py

--- sim_SNe.py
+++ sim_SNe.py
@@ -100,7 +100,7 @@ class ZoneLayerSupernova:
         for spine in self.ax_lc.spines.values():
             spine.set_color("black")
         self.ax_lc.set_ylim(0, 1.5 * LP_PHYS) 
-        self.ax_lc.set_yscale('log')
+        #self.ax_lc.set_yscale('log')
 
         self.ax_lc.set_xlim(0, TOTAL_FRAMES)
         self.ax_lc.grid(True, alpha=0.3)
@@ -245,6 +245,7 @@ with col2:
 
         progress_bar.progress(1.0)
         st.success("🎉 Simulation Complete! The star has gone supernova.")
+
 
 
 
